54b7c7ecfaf6892b1c6cb8e92454b7398dabc7cb
rudra-404 <rudrakaiser840@gmail.com>
2024-12-20 18:56:50 +0600

    Assignment

--- Assignment 02/Counter/counter.dart
+++ Assignment 02/Counter/counter.dart
@@ -1,21 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Counter(),
-    );
-  }
-}
-
 class Counter extends StatefulWidget {
   const Counter({super.key});
 
--- Assignment 02/Layout/layout.dart
+++ Assignment 02/Layout/layout.dart
@@ -1,21 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Layout(),
-    );
-  }
-}
-
 class Layout extends StatelessWidget {
   const Layout({super.key});
 
